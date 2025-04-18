aca64bc2801d86a4a9a8913fa4503761ba429ec1
zhaksyb <you@example.com>
2025-04-18 09:27:26 +0500

    language switch in nav bar

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ import About from './screens/about';
 import Converter from './screens/converter';
 import Account from './screens/account';
 import { useTheme } from './theme/theme';
+import { useTranslation } from 'react-i18next';
 
 const aboutIcon = require('../assets/images/information.png');
 const converterIcon = require('../assets/images/math.png');
@@ -21,6 +22,7 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function App() {
   const { theme } = useTheme();
+  const { t } = useTranslation();
 
   return (
     <Tab.Navigator
@@ -57,9 +59,21 @@ export default function App() {
     headerShown: false,
   })}
 >
-  <Tab.Screen name="About" component={About} />
-  <Tab.Screen name="Converter" component={Converter} />
-  <Tab.Screen name="Account" component={Account} />
-</Tab.Navigator>
+<Tab.Screen
+        name="About"
+        component={About}
+        options={{ tabBarLabel: t('about') }} 
+      />
+      <Tab.Screen
+        name="Converter"
+        component={Converter}
+        options={{ tabBarLabel: t('converter') }} 
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{ tabBarLabel: t('account') }} 
+      />
+    </Tab.Navigator>
   );
 }
